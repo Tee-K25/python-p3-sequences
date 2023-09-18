3b3313ca8b53bfcc059e4bbd8a97b403e8197042
Tony <tony.kiplagat@student.moringaschool.com>
2023-09-19 00:58:04 +0300

    Fibonacci'd

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib=list()
+    a,b = 0,1
+    for n in range(length):
+        fib.append(a)
+        a,b = b, a+b
+
+    print(fib)
+        
